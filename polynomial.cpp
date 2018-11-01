debe8de5077e5334087df6f6415d802130dda40c
Alex Koryakov <31795607+DoomzD@users.noreply.github.com>
2018-11-01 06:12:46 +0300

    Update polynomial.cpp

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,315 +1,232 @@
-#include <iostream>
 #include <vector>
-
+#include <iostream>
 
 template<typename T>
 class Polynomial {
 public:
-    Polynomial(std::vector<T>& v) : v_(v) {}
-    Polynomial(T num) : v_({num}) {}
-    Polynomial() : v_({}) {}
+    Polynomial() = default;
+
+    Polynomial(T num)
+            : Data({num}) {}
+
+    explicit Polynomial(const std::vector<T> &data)
+            : Data(data) {}
+
+    explicit Polynomial(const std::initializer_list<T> &data)
+            : Data(data) {}
 
     template<typename Iter>
     Polynomial(Iter begin, Iter end) {
         while (begin != end) {
-            v_.push_back(*(begin++));
+            Data.push_back(*(begin++));
         }
-    }
-
-    bool operator == (const Polynomial<T>& other) const;
-    bool operator != (const Polynomial<T>& other) const;
-
-    template<typename T1>
-    friend Polynomial<T1> operator + (const Polynomial<T1>& lhs, const Polynomial<T1>& rhs);
-
-    template<typename T1>
-    friend Polynomial<T1> operator - (const Polynomial<T1>& lhs, const Polynomial<T1>& rhs);
-
-    template<typename T1>
-    friend Polynomial<T1> operator & (const Polynomial<T1>& lhs, const Polynomial<T1>& rhs);
-
-    template<typename T1>
-    friend Polynomial<T1> operator * (const Polynomial<T1>& lhs, const Polynomial<T1>& rhs);
-
-    template<typename T1>
-    friend Polynomial<T1> operator / (const Polynomial<T1>& lhs, const Polynomial<T1>& rhs);
-
-    template<typename T1>
-    friend Polynomial<T1> operator % (const Polynomial<T1>& lhs, const Polynomial<T1>& rhs);
-
-    Polynomial<T>& operator += (const Polynomial<T>& other);
-    Polynomial<T>& operator -= (const Polynomial<T>& other);
-    Polynomial<T>& operator *= (const Polynomial<T>& other);
-
-    template<typename T1>
-    friend std::ostream& operator << (std::ostream& out, const Polynomial<T1>& cur);
-
-    const T operator[] (int pos) const {
-        return (static_cast<int>(v_.size()) > pos ? v_[pos] : T(0));
     }
 
     int Degree() const {
-        int sz = static_cast<int>(v_.size());
-        while (sz > 0 && v_[sz - 1] == T(0))
+        int sz = static_cast<int>(Data.size());
+        while (sz > 0 && Data[sz - 1] == T{0})
             --sz;
-        return (sz == 0 ? -1 : sz - 1);
+        return sz - 1;
     }
 
-    T operator () (T num) const;
+    const T operator[] (int pos) const {
+        return (static_cast<int>(Data.size()) > pos ? Data[pos] : T{0});
+    }
 
-    typename std::vector<T>::const_iterator begin() const;
+    friend bool operator==(const Polynomial& lhs, const Polynomial& rhs) {
+        if (lhs.Degree() != rhs.Degree())
+            return false;
+        for (int i = 0; i <= lhs.Degree(); ++i) {
+            if (lhs.Data[i] != rhs.Data[i])
+                return false;
+        }
+        return true;
+    }
 
-    typename std::vector<T>::const_iterator end() const;
+    friend bool operator!=(const Polynomial& lhs, const Polynomial& rhs) {
+        return !(lhs == rhs);
+    }
 
-    Polynomial<T> operator , (const Polynomial<T>& other) const;
+    Polynomial<T>& operator += (const Polynomial<T>& other) {
+        for (std::size_t i = 0; i < other.Data.size(); ++i) {
+            if (Data.size() > i) {
+                Data[i] += other.Data[i];
+            } else {
+                Data.push_back(other.Data[i]);
+            }
+        }
+        return *this;
+    }
+
+    Polynomial<T>& operator -= (const Polynomial<T>& other) {
+        for (std::size_t i = 0; i < other.Data.size(); ++i) {
+            if (Data.size() > i) {
+                Data[i] -= other.Data[i];
+            } else {
+                Data.push_back(T {-1} * other.Data[i]);
+            }
+        }
+        return *this;
+    }
+
+    Polynomial<T>& operator *= (const Polynomial<T>& other) {
+        std::vector<T> result(Degree() + other.Degree() + 2, T{0});
+
+        for (int i = 0; i <= Degree(); ++i) {
+            for (int j = 0; j <= other.Degree(); ++j) {
+                result[i + j] += Data[i] * other.Data[j];
+            }
+        }
+
+        Data = result;
+        return *this;
+    }
+
+    friend Polynomial operator + (Polynomial lhs, const Polynomial& rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+    friend Polynomial operator - (Polynomial lhs, const Polynomial& rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+    friend Polynomial operator * (Polynomial lhs, const Polynomial& rhs) {
+        lhs *= rhs;
+        return lhs;
+    }
+    friend Polynomial operator & (const Polynomial& lhs, const Polynomial& rhs) {
+        std::vector<T> result(lhs.Degree() * rhs.Degree() + 1, T{0});
+
+        for (int i = 0; i <= lhs.Degree(); ++i) {
+            int power = i;
+            Polynomial temp{T{1}};
+
+            while (power--) {
+                temp *= rhs;
+            }
+
+            for (int j = 0; j <= temp.Degree(); ++j) {
+                result[j] += temp[j] * lhs[i];
+            }
+        }
+
+        return Polynomial{result};
+    }
+    friend Polynomial operator / (const Polynomial& lhs, const Polynomial& rhs) {
+        Polynomial ans = Divide(lhs, rhs).first;
+        return ans;
+    }
+    friend Polynomial operator % (const Polynomial& lhs, const Polynomial& rhs) {
+        Polynomial ans = Divide(lhs, rhs).second;
+        return ans;
+    }
+    friend Polynomial operator , (const Polynomial<T>& lhs, const Polynomial& rhs) {
+        if (rhs == T{0}) {
+            std::vector<T> highesCoeff(1, T{0});
+            highesCoeff[0] = lhs[lhs.Degree()];
+            return lhs / Polynomial(highesCoeff);
+        }
+        return rhs, (lhs % rhs);
+    }
+
+    T operator () (T num) const {
+        T result = T{0};
+        T power = T{1};
+
+        for (int i = 0; i <= Degree(); ++i, power *= num) {
+            result += Data[i] * power;
+        }
+
+        return result;
+    }
+
+    typename std::vector<T>::const_iterator begin() const {
+        return Data.cbegin();
+    }
+
+    typename std::vector<T>::const_iterator end() const {
+        return Data.cbegin() + Degree() + 1;
+    }
+
+    template<typename U>
+    friend std::ostream& operator << (std::ostream& out, const Polynomial<U>& data);
 
 private:
-    std::vector<T> v_;
+    friend std::pair<Polynomial<T>, Polynomial<T>>
+    Divide(const Polynomial<T>& lhs, const Polynomial<T>& rhs) {
+        if (lhs.Degree() < rhs.Degree())
+            return {T{0}, lhs};
+
+        Polynomial<T> temp{lhs.Data};
+        std::vector<T> result(lhs.Degree() - rhs.Degree() + 1, T{0});
+
+        while (temp.Degree() >= rhs.Degree()) {
+            T coeff = temp[temp.Degree()] / rhs[rhs.Degree()];
+            result[temp.Degree() - rhs.Degree()] += coeff;
+
+            std::vector<T> subtract(temp.Degree() - rhs.Degree() + 1, T{0});
+            subtract[temp.Degree() - rhs.Degree()] += coeff;
+            temp -= Polynomial {subtract} * rhs;
+        }
+
+        return {Polynomial{result}, temp};
+    }
+    T AbsoluteValue(const T& elem) const {
+        return elem < T{0} ? elem * T{-1} : elem;
+    }
+    std::string Sign(int pos, int deg) const {
+        if (Data[pos] < T{0}) {
+            return "-";
+        } else {
+            if (pos != deg)
+                return "+";
+            return "";
+        }
+    }
+    std::ostream& Coeff(int pos, std::ostream& out) const {
+        if (Data[pos] == T{1} || Data[pos] == T{-1}) {
+            if (pos == 0)
+                return out << "1";
+            return out << "";
+        } else {
+            if (pos == 0)
+                return out << AbsoluteValue(Data[pos]);
+            out << AbsoluteValue(Data[pos]);
+            return out << "*";
+        }
+    }
+    std::string Var(int pos) const {
+        if (pos != 0)
+            return "x";
+        return "";
+    }
+    std::string Power(int pos) const {
+        if (pos > 1)
+            return "^" + std::to_string(pos);
+        return "";
+    }
+
+    std::vector<T> Data;
 };
 
 template<typename T>
-T ab(T& arg) {
-    if (arg < T(0))
-        return arg * T(-1);
-    return arg;
-}
-
-template<typename T>
-bool Polynomial<T>::operator==(const Polynomial<T> &other) const {
-    if (Degree() != other.Degree())
-        return false;
-    for (int i = 0; i <= Degree(); ++i) {
-        if (v_[i] != other.v_[i])
-            return false;
-    }
-    return true;
-}
-
-template<typename T>
-bool Polynomial<T>::operator!=(const Polynomial<T> &other) const {
-    return !(*this == other);
-}
-
-template<typename T>
-Polynomial<T> operator+(const Polynomial<T> &lhs, const Polynomial<T> &rhs) {
-    Polynomial<T> temp = lhs;
-    return temp += rhs;
-}
-
-template<typename T>
-Polynomial<T> operator-(const Polynomial<T> &lhs, const Polynomial<T> &rhs) {
-    Polynomial<T> temp = lhs;
-    return temp -= rhs;
-}
-
-template<typename T>
-Polynomial<T> &Polynomial<T>::operator+=(const Polynomial<T> &other) {
-    for (size_t i = 0; i < other.v_.size(); ++i) {
-        if (v_.size() > i)
-            v_[i] += other.v_[i];
-        else
-            v_.push_back(other.v_[i]);
-    }
-    return *this;
-}
-
-template<typename T>
-Polynomial<T> &Polynomial<T>::operator-=(const Polynomial<T> &other) {
-    for (size_t i = 0; i < other.v_.size(); ++i) {
-        if (v_.size() > i) {
-            v_[i] -= other.v_[i];
-        } else {
-            v_.push_back(other.v_[i]);
-            v_[v_.size() - 1] *= T(-1);
-        }
-    }
-    return *this;
-}
-
-template <typename T>
-typename std::vector<T>::const_iterator Polynomial<T>::begin() const {
-    return v_.begin();
-}
-
-template <typename T>
-typename std::vector<T>::const_iterator Polynomial<T>::end() const {
-    return (v_.begin() + Degree() + 1);
-}
-
-template<typename T>
-std::ostream &operator<<(std::ostream &out, const Polynomial<T> &cur) {
-    if (cur.Degree() == -1) {
+std::ostream& operator << (std::ostream& out, const Polynomial<T>& data) {
+    int deg = data.Degree();
+    if (deg == -1) {
         out << 0;
         return out;
     }
-    for (int i = cur.Degree(); i >= 0; --i) {
-        if (cur.v_[i] == T(0))
+
+    for (int i = deg; i >= 0; --i) {
+        if (data[i] == T{0})
             continue;
-        if (i == 0) {
-            if (i != cur.Degree()) {
-                if (cur.v_[i] < T(0)) {
-                    out << cur.v_[i];
-                } else {
-                    out << '+' << cur.v_[i];
-                }
-            } else {
-                out << cur.v_[i];
-            }
-            return out;
-        }
-        if (i == cur.Degree()) {
-            if (ab(cur.v_[i]) == T(1)) {
-                if (cur.v_[i] < T(0)) {
-                    if (i != 1) {
-                        out << "-x^" << i;
-                    } else {
-                        out << "-x";
-                    }
-                } else {
-                    if (i != 1) {
-                        out << "x^" << i;
-                    } else {
-                        out << "x";
-                    }
-                }
-            } else {
-                if (i != 1) {
-                    out << cur.v_[i] << "*x^" << i;
-                } else {
-                    out << cur.v_[i] << "*x";
-                }
-            }
-        } else {
-            if (ab(cur.v_[i]) == T(1)) {
-                if (cur.v_[i] < T(0)) {
-                    if (i != 1) {
-                        out << "-x^" << i;
-                    } else {
-                        out << "-x";
-                    }
-                } else {
-                    if (i != 1) {
-                        out << '+' << "x^" << i;
-                    } else {
-                        out << '+' << "x";
-                    }
-                }
-            } else {
-                if (cur.v_[i] < T(0)) {
-                    if (i != 1) {
-                        out << cur.v_[i] << "*x^" << i;
-                    } else {
-                        out << cur.v_[i] << "*x";
-                    }
-                } else {
-                    if (i != 1) {
-                        out << '+' << cur.v_[i] << "*x^" << i;
-                    } else {
-                        out << '+' << cur.v_[i] << "*x";
-                    }
-                }
-            }
-        }
+
+        std::string currentValue;
+        out << data.Sign(i, deg);
+        data.Coeff(i, out);
+        out << data.Var(i);
+        out << data.Power(i);
     }
+
     return out;
-}
-
-template<typename T>
-Polynomial<T> operator * (const Polynomial<T>& lhs, const Polynomial<T>& rhs) {
-    Polynomial<T> temp = lhs;
-    return temp *= rhs;
-}
-
-template<typename T>
-Polynomial<T> &Polynomial<T>::operator*=(const Polynomial<T> &other) {
-    std::vector<T> result(other.Degree() + Degree() + 2, T(0));
-    for (int i = 0; i <= Degree(); ++i) {
-        for (int j = 0; j <= other.Degree(); ++j) {
-            result[i + j] += v_[i] * other.v_[j];
-        }
-    }
-    (*this).v_ = result;
-    return *this;
-}
-
-template<typename T>
-T Polynomial<T>::operator()(T num) const {
-    T result = T(0);
-    T curPow = T(1);
-    for (int i = 0; i <= Degree(); ++i, curPow *= num) {
-        result += v_[i] * curPow;
-    }
-    return result;
-}
-
-template<typename T1>
-Polynomial<T1> operator&(const Polynomial<T1> &lhs, const Polynomial<T1> &rhs) {
-    std::vector<T1> result(lhs.Degree() * rhs.Degree() + 1, T1(0));
-    for (int i = 0; i <= lhs.Degree(); ++i) {
-        int curDegree = i;
-        Polynomial<T1> temp(T1(1));
-        while (curDegree > 0) {
-            temp *= rhs;
-            --curDegree;
-        }
-        for (int j = 0; j <= temp.Degree(); ++j) {
-            result[j] += lhs.v_[i] * temp.v_[j];
-        }
-    }
-    return {result};
-}
-
-template<typename T1>
-Polynomial<T1> operator/(const Polynomial<T1> &lhs, const Polynomial<T1> &rhs) {
-    if (lhs.Degree() < rhs.Degree()) {
-        return {T1(0)};
-    }
-    Polynomial<T1> temp = lhs;
-    std::vector<T1> result(lhs.Degree() - rhs.Degree() + 1, T1(0));
-    while (temp.Degree() >= rhs.Degree()) {
-        T1 cff = temp.v_[temp.Degree()] / rhs.v_[rhs.Degree()];
-        result[temp.Degree() - rhs.Degree()] += cff;
-        std::vector<T1> curCff(temp.Degree() - rhs.Degree() + 1, T1(0));
-        curCff[temp.Degree() - rhs.Degree()] += cff;
-        temp -= (rhs * Polynomial<T1>(curCff));
-    }
-    return {result};
-}
-
-template<typename T1>
-Polynomial<T1> operator%(const Polynomial<T1> &lhs, const Polynomial<T1> &rhs) {
-    if (lhs.Degree() < rhs.Degree()) {
-        return {T1(0)};
-    }
-    Polynomial<T1> temp = lhs;
-    std::vector<T1> result(lhs.Degree() - rhs.Degree() + 1, T1(0));
-    while (temp.Degree() >= rhs.Degree()) {
-        T1 cff = temp.v_[temp.Degree()] / rhs.v_[rhs.Degree()];
-        result[temp.Degree() - rhs.Degree()] += cff;
-        std::vector<T1> curCff(temp.Degree() - rhs.Degree() + 1, T1(0));
-        curCff[temp.Degree() - rhs.Degree()] += cff;
-        temp -= (rhs * Polynomial<T1>(curCff));
-    }
-    return temp;
-}
-
-template<typename T>
-Polynomial<T> Polynomial<T>::operator,(const Polynomial<T> &other) const {
-    Polynomial<T> num;
-    Polynomial<T> den;
-    if (Degree() >= other.Degree()) {
-        num = *this;
-        den = other;
-    } else {
-        num = other;
-        den = *this;
-    }
-    Polynomial<T> temp = num % den;
-    while (temp != T(0)) {
-        num = den;
-        den = temp;
-        temp = num % den;
-    }
-    return (den / Polynomial<T>(static_cast<T>(den[den.Degree()])));
-    return den;
 }
